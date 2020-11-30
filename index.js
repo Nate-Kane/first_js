cc3bc634afa439637ea4b6ddd120fc2d1281d070
Nathan Kane <kaneitalia.rana@gmail.com>
2020-11-30 14:24:48 -0800

    first commit && made some variables

---
+++ index.js
@@ -0,0 +1,4 @@
+let guitar0 = { brand: "Fender", model: "Stratocaster", year: "2015", price: 1600 }
+let guitar1 = { brand: "Fender", model: "Telecaster", year: "2012", price: 1100 }
+let guitar2 = { brand: "Gibson", model: "Les Paul", year: "1985", price: 2500 }
+
